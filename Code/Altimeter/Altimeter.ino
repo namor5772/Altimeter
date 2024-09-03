1c2d84375ef41fc0720418279d95ab3fb52e7df0
roman groblicki <romangroblicki@proton.me>
2024-09-03 10:16:57 +1000

    From Laptop A

--- Code/Altimeter/Altimeter.ino
+++ Code/Altimeter/Altimeter.ino
@@ -72,10 +72,10 @@ void loop()
   
   // Display the data
   
-  dtostrf(altRel,13,1,altstr);
+  dtostrf(altRel,8,1,altstr);
   u8x8.drawString(0,0,altstr);
 
-  dtostrf(alt,13,1,altstr2);
+  dtostrf(alt,8,1,altstr2);
   u8x8.drawString(0,2,altstr2);
 
   dtostrf(temp,8,1,tempstr);
