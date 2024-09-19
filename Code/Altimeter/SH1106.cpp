9dd18af4ecd7a9e633c1d07fbecf8690190ac922
roman groblicki <romangroblicki@proton.me>
2024-09-20 08:33:16 +1000

    Laptop 20/9/24 A

---
+++ Code/Altimeter/SH1106.cpp
@@ -0,0 +1,247 @@
+#include "Arduino.h"
+#include "SH1106.h"
+#include "SH1106_font.h"
+
+// Constructor
+SH1106::SH1106(uint8_t clk, uint8_t mos, uint8_t res, uint8_t dc, uint8_t cs) {
+  clkPin = clk;
+  mosPin = mos;
+  resPin = res;
+  dcPin = dc;
+  csPin = cs;
+
+  dataMode = false;
+  initialized = false;
+
+  graphicsBuffer = nullptr;
+  clearDisplayRamOnNextDisplay = true;
+} 
+
+void SH1106::initPins() {
+  pinMode(resPin, OUTPUT); 
+  digitalWrite(resPin, HIGH);
+  pinMode(clkPin, OUTPUT);
+  pinMode(mosPin, OUTPUT);
+  pinMode(dcPin, OUTPUT);
+  pinMode(csPin, OUTPUT);
+}
+
+void SH1106::reset() {
+  // hold res pin LOW for a while 
+  digitalWrite(resPin, LOW); 
+  delayMicroseconds(10);
+  digitalWrite(resPin, HIGH); //delayMicroseconds(10);
+}
+
+void SH1106::TransferStart() {
+  digitalWrite(csPin, LOW);  // CS (Upper score) 
+}
+
+void SH1106::TransferEnd() {
+  digitalWrite(csPin, HIGH);
+}
+
+void SH1106::CommandMode() {
+  dataMode = false; // =0
+  digitalWrite(dcPin, LOW); // A0 set to LOW
+}
+
+void SH1106::DataMode() {
+  dataMode = true; // =1
+  digitalWrite(dcPin, HIGH); // A0 set to HIGH
+}
+
+// bit-bashed SPI write a byte code using cls and mos pins
+void SH1106::Write(uint8_t d) {
+  for (int i=7; i>=0; --i) {
+    digitalWrite(clkPin, LOW);
+    digitalWrite(mosPin, d >> i & 1);
+    digitalWrite(clkPin, HIGH);
+  }
+}
+
+void SH1106::init() {
+  if (!initialized) { 
+    initPins();
+    reset();
+    clearDisplayRam();
+    configureDefault();
+    initialized = true;
+  }
+}
+
+void SH1106::clearDisplayRam() {
+  TransferStart();
+  CommandMode();
+  Write(SH1106_SET_COLUMN_ADDR_LOW | 2);
+  for (uint8_t page = 0; page < 8; page++) {
+    CommandMode();
+    Write(SH1106_SET_PAGE_ADDR | page);
+    Write(SH1106_SET_COLUMN_ADDR_HIGH | 0);
+    DataMode();
+    for (uint8_t i = 0; i < 128; i++) Write(0);
+  }
+  TransferEnd();
+  clearDisplayRamOnNextDisplay = false;
+}
+
+void SH1106::configureDefault() {
+  TransferStart();
+  CommandMode();
+  Write(SH1106_SET_SEGMENT_REMAP | 1);  // flip horizontally
+  Write(SH1106_SET_COM_SCAN_DIR | 8);   // flip vertically
+  Write(SH1106_SET_CONTRAST);
+  Write(0xFF);                          // maximum brightness
+  Write(SH1106_SET_DISPLAY_ON | 1);
+  TransferEnd();
+}
+
+void SH1106::invert() {
+  init();
+  TransferStart();
+  CommandMode();
+  Write(SH1106_SET_INVERT_DISPLAY | 1);
+  TransferEnd();
+}
+
+void SH1106::rotate180() {
+  init();
+  TransferStart();
+  CommandMode();
+  Write(SH1106_SET_SEGMENT_REMAP);
+  Write(SH1106_SET_COM_SCAN_DIR);
+  TransferEnd();
+}
+
+void SH1106::display8x2Text() {
+  TransferStart();
+  writeSegmentUpperText(2, 2 + 17, getLinePointer(0), 8);
+  writeSegmentLowerText(3, 2 + 17, getLinePointer(0), 8);
+  writeSegmentUpperText(5, 2 + 17, getLinePointer(1), 8);
+  writeSegmentLowerText(6, 2 + 17, getLinePointer(1), 8);
+  TransferEnd();
+}
+
+void SH1106::display8x2TextPartial(uint8_t x, uint8_t y, uint8_t width) {
+    if (x >= 8 || y >= 2) { return; }
+    if (width > (uint8_t)(8 - x)) { width = 8 - x; }
+    if (width == 0) { return; }
+
+    const uint8_t page = 2 + y * 3;
+    const uint8_t columnAddr = 2 + 17 + x * 12;
+    const uint8_t * const text = getLinePointer(y) + x;
+
+    TransferStart();
+    writeSegmentUpperText(page, columnAddr, text, width);
+    writeSegmentLowerText(page + 1, columnAddr, text, width);
+    TransferEnd();
+  }
+
+void SH1106::displayPartial(uint8_t x, uint8_t y, uint8_t width) {
+  init();
+  if (clearDisplayRamOnNextDisplay) { clearDisplayRam(); }
+  display8x2TextPartial(x, y, width);
+}
+
+void SH1106::writeSegmentUpperText(uint8_t page, uint8_t columnAddr, const uint8_t * text, uint8_t textLength) {
+  CommandMode();
+  Write(SH1106_SET_PAGE_ADDR | page);
+  Write(SH1106_SET_COLUMN_ADDR_HIGH | (columnAddr >> 4));
+  Write(SH1106_SET_COLUMN_ADDR_LOW | (columnAddr & 0xF));
+  DataMode();
+  for (uint8_t i = 0; i < textLength; i++) {
+    uint8_t glyph = *text++;
+    for (uint8_t pixelX = 0; pixelX < 5; pixelX++) {
+      uint8_t column = repeatBits(getGlyphColumn(glyph, pixelX) & 0xF);
+//          column = 0xFF;
+      Write(column); Write(column);
+    }
+    Write(0); Write(0);
+  }
+}
+
+void SH1106::writeSegmentLowerText(uint8_t page, uint8_t columnAddr, const uint8_t * text, uint8_t textLength) {
+    CommandMode();
+    Write(SH1106_SET_PAGE_ADDR | page);
+    Write(SH1106_SET_COLUMN_ADDR_HIGH | (columnAddr >> 4));
+    Write(SH1106_SET_COLUMN_ADDR_LOW | (columnAddr & 0xF));
+    DataMode();
+    for (uint8_t i = 0; i < textLength; i++) {
+      uint8_t glyph = *text++;
+      for (uint8_t pixelX = 0; pixelX < 5; pixelX++) {
+        uint8_t column = repeatBits(getGlyphColumn(glyph, pixelX) >> 4);
+//        column = 0xFF;
+        Write(column); Write(column);
+      }
+      Write(0); Write(0);
+    }
+  }
+
+uint8_t SH1106::getGlyphColumn(uint8_t glyph, uint8_t pixelX) {
+  if (glyph >= 0x20) {
+    return pgm_read_byte(&Font[glyph - 0x20][pixelX]);
+  }
+  /*
+  else if (glyph < 8) {
+    return customChars[glyph][pixelX];
+  }
+  */
+  else {
+    return 0;
+  }
+}
+
+uint8_t * SH1106::getLinePointer(uint8_t line) {
+  return textBuffer + line * textBufferWidth;
+}
+
+void SH1106::gotoXY(uint8_t x, uint8_t y) {
+  textCursorX = x;
+  textCursorY = y;
+}
+
+size_t SH1106::write(uint8_t d) {
+  if (textCursorY >= textBufferHeight) { return 0; }
+  if (textCursorX >= textBufferWidth) { return 0; }
+
+  *(getLinePointer(textCursorY) + textCursorX) = d;
+
+  if (!disableAutoDisplay) {
+    displayPartial(textCursorX, textCursorY, 1);
+  }
+
+  textCursorX++;
+  return 1;
+}
+
+
+void SH1106::writeBlock(uint8_t page, uint8_t columnAddr, uint8_t columnNum, uint8_t column) {
+  TransferStart();
+  CommandMode();
+  Write(SH1106_SET_PAGE_ADDR | page);
+  Write(SH1106_SET_COLUMN_ADDR_HIGH | (columnAddr >> 4));
+  Write(SH1106_SET_COLUMN_ADDR_LOW | (columnAddr & 0xF));
+  DataMode();
+  for (uint8_t i = 0; i < columnNum; i++) {
+    Write(column);
+  }  
+  TransferEnd();
+}
+
+// After writing to screen need to call this to stop cursor?
+void SH1106::writeEND() {
+  TransferStart();
+  CommandMode();
+  Write(SH1106_SET_PAGE_ADDR | 0);
+  Write(SH1106_SET_COLUMN_ADDR_HIGH | 0);
+  Write(SH1106_SET_COLUMN_ADDR_LOW | 0);
+  DataMode();
+  Write(0);
+  TransferEnd();
+}
+
+// The only function that is not in the SH1106 class
+static inline uint8_t repeatBits(uint8_t d) {
+  return pgm_read_byte(repeatBitsTable + d);
+}
+
---
+++ Code/Altimeter/SH1106.h
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+
+#define SH1106_SET_COLUMN_ADDR_LOW 0x00
+#define SH1106_SET_COLUMN_ADDR_HIGH 0x10
+#define SH1106_SET_CONTRAST 0x81
+#define SH1106_SET_SEGMENT_REMAP 0xA0
+#define SH1106_SET_INVERT_DISPLAY 0xA6
+#define SH1106_SET_DISPLAY_ON 0xAE
+#define SH1106_SET_PAGE_ADDR 0xB0
+#define SH1106_SET_COM_SCAN_DIR 0xC0
+
+static inline uint8_t repeatBits(uint8_t d);
+
+class SH1106 {
+  typedef unsigned char uchar;
+
+  public:
+/*  
+ The module (in 4-wire SPI mode) has 7 pins (which on the Arduino PRO Mini with HW SPI use):
+ 1 GND - connect to ground pin on micro (Arduino Pro Mini 5V 16Mhz assumed here)
+ 2 VCC - connect to VCC pin on micro (assumed 5V)
+ 3 CLK - Clock, connect to Pin 13 on micro (assumed by default) - WIRE 1 
+       - Called SCL Wire 3 in SH1106 pdf
+  MISO - Master In Slave Out, NOT AVAILABLE HERE - but would be pin 12 on micro - WIRE 3 
+ 4 MOSI - Master Out Slave In, connect to pin 11 on micro (assumed by default) - WIRE 2
+        - also called SDI or SI Wire 2 in SH1106 pdf
+ 5 RES - Reset, connect to Pin 8 (can change) 
+       - Called RES (Upper score) in SH1106 pdf
+ 6 DC - Data/Command, connect to pin 9 (can change)
+      - Called A0 (HIGH= data mode, LOW= command mode) Wire 4 in SH1106 pdf 
+ 7 CS - Chip Select, connect to pin 10 (can change) - WIRE 4 
+      - Called CS (upper score) Wire 1 in SH1106 pdf
+*/
+  SH1106(uint8_t clk, uint8_t mos, uint8_t res, uint8_t dc, uint8_t cs); // Constructor
+
+  private:
+  
+  uint8_t clkPin, mosPin, resPin, dcPin, csPin;
+  bool initialized;
+  bool dataMode;
+  bool clearDisplayRamOnNextDisplay;
+  bool disableAutoDisplay;
+  static const uint8_t textBufferWidth = 21, textBufferHeight = 8;
+  uint8_t textBuffer[textBufferHeight * textBufferWidth];
+  uint8_t textCursorX;
+  uint8_t textCursorY;
+  const uint8_t * graphicsBuffer;
+
+  // low level SPI comms with SH1106
+  void initPins();
+  void reset();
+  void TransferStart();
+  void TransferEnd();
+  void CommandMode();
+  void DataMode();
+  void Write(uint8_t d);
+  void init();
+  void clearDisplayRam();
+  void configureDefault();
+
+  void writeSegmentUpperText(uint8_t page, uint8_t columnAddr, const uint8_t * text, uint8_t textLength);
+  void writeSegmentLowerText(uint8_t page, uint8_t columnAddr, const uint8_t * text, uint8_t textLength);
+  uint8_t getGlyphColumn(uint8_t glyph, uint8_t pixelX);
+  uint8_t * getLinePointer(uint8_t line);
+
+  void display8x2Text(); // = displayFunction
+  void display8x2TextPartial(uint8_t x, uint8_t y, uint8_t width); // = displayPartialFunction 
+  void displayPartial(uint8_t x, uint8_t y, uint8_t width);
+  
+
+  public:
+
+  void invert();
+  void rotate180();
+  void gotoXY(uint8_t x, uint8_t y);
+  size_t write(uint8_t d);
+  void writeBlock(uint8_t page, uint8_t columnAddr, uint8_t columnNum, uint8_t column);
+  void writeEND();
+};
+
